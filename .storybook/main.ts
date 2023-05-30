dddad89cf67e9a4361751dc4fd4c6eabd3ba6d95
Alex Hall <alex.hall@codecomputerlove.com>
2023-05-30 16:39:30 +0100

    build(deps): upgraded Storybook and add fixes for static assets dir (#435)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
+import path from 'path';
+
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: [
@@ -16,6 +18,19 @@ const config: StorybookConfig = {
 	},
 	docs: {
 		autodocs: true,
+	},
+	staticDirs: ['../public'],
+	webpackFinal: async (config) => {
+		if (config.resolve) {
+			// Make custom items in `public` available to the app.
+			config.resolve.roots = [
+				path.resolve(__dirname, '../public'),
+				'node_modules',
+			];
+		}
+
+		// Return the altered config
+		return config;
 	},
 };
 export default config;
